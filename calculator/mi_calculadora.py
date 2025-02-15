6f436276a9a40bfa136cdcef0cdf72b694f6842a
Adrián Lancho <adlanchod@alumnos.unex.es>
2025-02-15 21:13:07 +0100

    Nuevas modificaciones en 'mi_calculadora.py'

--- calculator/mi_calculadora.py
+++ calculator/mi_calculadora.py
@@ -1,6 +1,6 @@
 # Custom function to read the two numbers.
 def read_numbers():
-    num4 = float(input('Enter first number: '))
+    num3 = float(input('Enter first number: '))
     num2 = float(input('Enter second number: '))
     return num1, num2
 
